d23571be8f136177c05df2a446e5070849cfc333
zzplumbzz <nicolasplumb@yahoo.com>
2019-09-24 10:28:31 -0400

    Check storage for 300MB

--- ActionEngine/Source/ActionEngine/ActionEngine/Initialize.cpp
+++ ActionEngine/Source/ActionEngine/ActionEngine/Initialize.cpp
@@ -1,5 +1,6 @@
 #include "Initialize.h"
 
+
 bool Initialize::IsOnlyInstance(const char* gameTitle , HANDLE* hHandle)
 {
 	*hHandle = CreateMutex(NULL, TRUE, gameTitle);
@@ -50,4 +51,47 @@ void Initialize::Terminate(HANDLE* hHandle)
 	CloseHandle(*hHandle);
 }
 
- 
+
+
+
+
+bool Initialize::CheckStorage(const DWORDLONG diskSpaceNeeded)
+{
+	// Check for enough free disk space on the current disk.
+
+	//int const drive = _getdrive();
+	//struct _diskfree_t diskfree;
+	//_getdiskfree(drive, &diskfree);
+	//cout << diskfree.total_clusters;
+
+	long long cluster = 0;
+	long long b = 0;
+	long long kb = 0;
+	long long mb = 0;
+	
+int const drive = _getdrive();
+		struct _diskfree_t diskfree;
+		_getdiskfree(drive, &diskfree);
+		b = diskfree.avail_clusters *diskfree.sectors_per_cluster;
+		b = b * diskfree.bytes_per_sector;
+		kb = b / 1024;
+		mb = kb / 1024;
+		//cout << b << " This Is B.\n" << endl;
+		//cout << kb << " This Is KB.\n" << endl;
+		cout << mb << " This Is MB.\n" << endl;
+
+		if (mb < 300)
+		{
+			MessageBox(NULL, " not enough space on harddrive", NULL, MB_OK);
+			return false;
+		}
+		return true;
+	
+}
+
+
+
+
+	
+
+
--- ActionEngine/Source/ActionEngine/ActionEngine/Source.cpp
+++ ActionEngine/Source/ActionEngine/ActionEngine/Source.cpp
@@ -11,6 +11,9 @@ int main()
 	if (!Initialize::CheckAvailibleMemory(300000, 300000)) // memory in kb
 		return(0);
 
+	if (!Initialize::CheckStorage(300)) // memory in kb
+		return 0;
+
 	while (true)
 	{
 	}
