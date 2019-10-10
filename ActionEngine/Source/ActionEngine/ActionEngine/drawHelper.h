213212fffd544c7139a44219d040338a25a94978
Ted-bissada <tbissada@gmail.com>
2019-10-09 21:22:59 -0400

    inlined vector2 operations

--- ActionEngine/Source/ActionEngine/ActionEngine/drawHelper.h
+++ ActionEngine/Source/ActionEngine/ActionEngine/drawHelper.h
@@ -4,15 +4,17 @@ struct v2
 {
 	v2(float _x =0, float _y=0) : x(_x), y(_y) {}
 	float x, y;
-	v2 operator+(const v2& other)
-	{
-		return v2(x + other.x, y + other.y);
-	}
-	v2 operator*(const float& other)
-	{
-		return v2(x * other, y * other);
-	}
 };
+inline v2 operator+(v2 a, v2 b)
+{
+	return v2(a.x + b.x, a.y+ b.y);
+}
+
+inline v2 operator*(v2 a, float b)
+{
+	return v2(a.x *b, a.y *b);
+}
+
 
 struct drawObject
 {
