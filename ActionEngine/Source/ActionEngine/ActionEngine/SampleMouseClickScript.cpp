5189c884b2db6576ec9cf1e543821b1e7949891f
Ted-bissada <tbissada@gmail.com>
2019-11-26 09:43:55 -0500

    changed mouseClickTest function

--- ActionEngine/Source/ActionEngine/ActionEngine/SampleMouseClickScript.cpp
+++ ActionEngine/Source/ActionEngine/ActionEngine/SampleMouseClickScript.cpp
@@ -7,13 +7,13 @@
 void SampleActorMouseClick::onStart(Actor * current, float dt)
 {
 	nextFunction = static_cast<void(ScriptInterface::*)(Actor *current, float dt)>(&SampleActorMouseClick::onTick);
-	ActionEngine::Instance()->getInputInterface()->bindEvent(LEFTMOUSEBUTTON, std::bind(&SampleActorMouseClick::mouseClickTest, this));
+	ActionEngine::Instance()->getInputInterface()->bindEvent(LEFTMOUSEBUTTON, std::bind(&SampleActorMouseClick::mouseClickTest, this, std::ref(ActionEngine::Instance()->getInputInterface()->mouseX), std::ref(ActionEngine::Instance()->getInputInterface()->mouseY)));
 	onTick(current, dt);
 }
 
-void SampleActorMouseClick::mouseClickTest()
+void SampleActorMouseClick::mouseClickTest(int mouseX,int mouseY)
 {
-	v2 spawnCoordinates(ActionEngine::Instance()->getInputInterface()->mouseX, ActionEngine::Instance()->getInputInterface()->mouseY);
+	v2 spawnCoordinates(mouseX,mouseY);
 	Actor* temp = new Actor();
 	temp->addComponent("transform", new DataInterface<v2>(spawnCoordinates));
 	temp->addComponent("testImage", new DrawSprite(drawObject(1, v2(0, 0)), 1));
