27738976b6d0656d85b9a191b71293134a4afe4b
Ted-bissada <tbissada@gmail.com>
2020-01-08 23:12:42 -0500

    event manager update
    -event manager is now a varadic template quite frankly im not sure if this is genius or idiotic

--- ActionEngine/Source/ActionEngine/ActionEngine/EventManager.h
+++ ActionEngine/Source/ActionEngine/ActionEngine/EventManager.h
@@ -2,22 +2,12 @@
 #include <unordered_map>
 #include <functional>
 #include <Windows.h>
+#include <algorithm>
 
-struct Tigr;
-
-enum class eventParameterTypes {
-	NONE,
-	INT2
-};
-
-typedef union eventData{
-	struct int2 { int i1, i2;}int2;
-};
-
+template <typename ...Args>
 struct EventCallbacks
 {
-	std::unordered_map<int, std::function<void(eventData)>> callbacks; //callback id , callback
-	eventParameterTypes parameters = eventParameterTypes::NONE;
+	std::unordered_map<int, std::function<void(Args...)>> callbacks; //callback id , callback
 };
 
 struct EventId 
@@ -26,24 +16,65 @@ struct EventId
 	int idNumber;
 };
 
+
+template <typename ...Args>
 class EventManager
 {
 private:
-	static EventManager* s_pInstance;
+	static EventManager<Args...>* s_pInstance;
 	EventManager() {}
-	std::unordered_map<std::string, EventCallbacks> events; //event name, callback list
+	typename std::unordered_map<std::string, EventCallbacks<Args...>> events; //event name, callback list
 	std::vector<EventId> pendingRemoval;
-	void removePendingEvents();
+	void removePendingEvents()
+	{
+		for (int i = 0; i < pendingRemoval.size(); i++)
+		{
+			events[pendingRemoval[i].name].callbacks.erase(pendingRemoval[i].idNumber);
+		}
+		pendingRemoval.clear();
+	}
 public:	
 	static EventManager* Instance()
 	{
 		if (s_pInstance == 0)
 		{
-			s_pInstance = new EventManager();
+			s_pInstance = new EventManager<Args...>();
 		}
 		return s_pInstance;
 	}
-	void fireEvent(std::string name,...);
-	EventId bindEvent(std::string, eventParameterTypes paramTypes,std::function<void(eventData)> func);
-	void unBindEvent(EventId);
+
+	void fireEvent(std::string name, Args ...args)
+	{
+		removePendingEvents();
+		EventCallbacks<Args...>* currentEvent = &events[name];
+		if (currentEvent->callbacks.size() == 0)
+			return;
+
+		typename std::unordered_map<int, typename std::function<void(Args...)>>::iterator it = currentEvent->callbacks.begin();
+		while (it != currentEvent->callbacks.end())
+		{
+			it->second(args...);
+			it++;
+		}
+	}
+
+	EventId bindEvent(std::string eventName, std::function<void(Args...)> func)
+	{
+		EventCallbacks<Args...>* currentEvent = &events[eventName];
+		EventId newEvent;
+		newEvent.name = eventName;
+		do {
+			newEvent.idNumber = rand();
+		} while (currentEvent->callbacks.count(newEvent.idNumber) > 0);
+
+		currentEvent->callbacks[newEvent.idNumber] = func;
+		return newEvent;
+	}
+	void unBindEvent(EventId id) 
+	{
+		pendingRemoval.push_back(id);
+	}
 };
+
+EventManager<>* EventManager<>::s_pInstance = nullptr;
+EventManager<int, int>* EventManager<int, int>::s_pInstance = nullptr;
