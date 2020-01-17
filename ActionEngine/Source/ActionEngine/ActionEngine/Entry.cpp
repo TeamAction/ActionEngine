641782dd567cea49bf002762b63fd96c8d78ab4f
Ted-bissada <tbissada@gmail.com>
2020-01-17 11:41:26 -0500

    added exports to request discrete gpu

--- ActionEngine/Source/ActionEngine/ActionEngine/Entry.cpp
+++ ActionEngine/Source/ActionEngine/ActionEngine/Entry.cpp
@@ -2,6 +2,20 @@
 #include "ActionEngine.h"
 #include "windows.h"
 
+/*
+//i added these because my computer was using my igpu not sure if they are really needed
+//use nvidia graphics adapter if available
+extern "C"
+{
+	__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+}
+//use amd graphics adapter if available
+extern "C"
+{
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+*///not gona bother with this unless igpu cant keep up
+
 int WINAPI WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -11,4 +25,3 @@ int WINAPI WinMain(
 	ActionEngine::Instance()->play();
 	return (0);
 }
- 
